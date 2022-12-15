40c3cce1b2127ffbc41d2c16ecbce93ed21481e7
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-16 00:57:03 +0500

    setup basic http server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,19 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import * as http from "http";
 
-console.log("alo 123");
+const main = async () => {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+    res.end("ok");
+  });
+
+  server.listen(process.env.RSS_APP_PORT, () => {
+    console.log(`Server listening on port: ${process.env.RSS_APP_PORT}`);
+  });
+};
+
+main().catch((error) => {
+  console.error("Backend failed to start up", error);
+  process.exit(1);
+});
